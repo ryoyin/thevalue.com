19d44fd263913bb219649d68fc41010f7ebe73a8
Roy Ho <kwanyin2000@gmail.com>
2016-12-27 21:56:41 +0800

    rollback featured news

--- resources/views/frontend/homepage/featured-news.blade.php
+++ resources/views/frontend/homepage/featured-news.blade.php
@@ -1,7 +1,22 @@
 <div class="row" id="featured-news">
     <ul class="col-md-3 ul-clean">
-        <li>IMG</li>
-        <li>TITLE</li>
-        <li>DESC</li>
+        <li><img src="{{ asset('images/articles/temp/article-01.jpg') }}" class="img-responsive"></li>
+        <li>Title 標題</li>
+        <li>Marvel’s The Inhumans Series Heads to IMAX Theaters and ABC!</li>
+    </ul>
+    <ul class="col-md-3 ul-clean">
+        <li><img src="{{ asset('images/articles/temp/article-02.jpg') }}" class="img-responsive"></li>
+        <li>Title 標題</li>
+        <li>Marvel’s Defenders Cast is Coming Together in New Photos</li>
+    </ul>
+    <ul class="col-md-3 ul-clean">
+        <li><img src="{{ asset('images/articles/temp/article-03.jpg') }}" class="img-responsive"></li>
+        <li>Title 標題</li>
+        <li>Full Justice League Movie Cast Revealed</li>
+    </ul>
+    <ul class="col-md-3 ul-clean">
+        <li><img src="{{ asset('images/articles/temp/article-04.jpg') }}" class="img-responsive"></li>
+        <li>Title 標題</li>
+        <li>Explore the Multiverse with Our Benedict Cumberbatch Doctor Strange Video Interview</li>
     </ul>
 </div> <!-- news-brief -->
