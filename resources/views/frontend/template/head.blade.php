0a418b5a8873f9005831cb060eeceeb8059aa493
Roy Ho <kwanyin2000@gmail.com>
2017-04-12 19:11:42 +0800

    Add site icons

--- resources/views/frontend/template/head.blade.php
+++ resources/views/frontend/template/head.blade.php
@@ -5,8 +5,6 @@
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="favicon.ico">
-
     <title>TheValue.com</title>
 
     <script
@@ -61,5 +59,22 @@
     <meta property="fb:app_id" content="{{ $fbMeta['app_id'] }}">
     {{--<meta property="fb:admins" content="1136380453091512">--}}
 
+    <!-- site icon -->
+    <link rel="apple-touch-icon" sizes="57x57" href="{{ asset("images/icons/apple-icon-57x57.png") }}">
+    <link rel="apple-touch-icon" sizes="60x60" href="{{ asset("images/icons/apple-icon-60x60.png") }}">
+    <link rel="apple-touch-icon" sizes="72x72" href="{{ asset("images/icons/apple-icon-72x72.png") }}">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset("images/icons/apple-icon-76x76.png") }}">
+    <link rel="apple-touch-icon" sizes="114x114" href="{{ asset("images/icons/apple-icon-114x114.png") }}">
+    <link rel="apple-touch-icon" sizes="120x120" href="{{ asset("images/icons/apple-icon-120x120.png") }}">
+    <link rel="apple-touch-icon" sizes="144x144" href="{{ asset("images/icons/apple-icon-144x144.png") }}">
+    <link rel="apple-touch-icon" sizes="152x152" href="{{ asset("images/icons/apple-icon-152x152.png") }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset("images/icons/apple-icon-180x180.png") }}">
+    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset("images/icons/android-icon-192x192.png") }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset("images/icons/favicon-32x32.png") }}">
+    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset("images/icons/favicon-96x96.png") }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset("images/icons/favicon-16x16.png") }}">
+    <link rel="manifest" href="{{ asset("images/icons/manifest.json") }}">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="{{ asset("images/icons/ms-icon-144x144.png") }}"> <meta name="theme-color" content="#ffffff">
 
 </head>
