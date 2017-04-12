a629b7817177f3c06ed9c86ab4cd0051cca18b0d
Roy Ho <kwanyin2000@gmail.com>
2017-04-12 18:38:47 +0800

    article remove empty value

--- resources/views/frontend/article/content-left.blade.php
+++ resources/views/frontend/article/content-left.blade.php
@@ -11,31 +11,43 @@ $('.article-shares').html($apiResult.article['shares']+ ' shares');--}}
     <li class='title' id="article-title">{{ $articleDetails['title'] }}</li>
     <li class='notes' id="article-note">{{ $articleDetails['note'] }}</li>
 
-    @if(!$appMode)
     <li>
         <ul class='misc ul-clean'>
-            <li class='pull-left'>by <span id="article-author">{{ $articleDetails['author'] }}</span> <span id="article-date">{{ $articleDetails['published_at'] }}</span> </li>
-            <li class='pull-right'>
-                <ul class='ul-clean share'>
-                    <li><i class='fa fa-envelope' aria-hidden='true'></i></li>
-                    <li><i class='fa fa-wechat' aria-hidden='true'></i></li>
-                    <li><i class='fa fa-weibo' aria-hidden='true'></i></li>
-                    <li><i class='fa fa-twitter' aria-hidden='true'></i></li>
-                    <li><i class='fa fa-facebook-f' aria-hidden='true'></i></li>
-                    <li><span class="article-shares">{{ $article['shares'] }} shares</span></li>
-                </ul>
+            <li class='pull-left'>
+                @if($articleDetails['author'] != "")
+                    by <span id="article-author">{{ $articleDetails['author'] }}</span>
+                @endif
+
+                 <span id="article-date">{{ $articleDetails['published_at'] }}</span>
             </li>
+            @if(!$appMode)
+                <li class='pull-right'>
+                    <ul class='ul-clean share'>
+                        <li><i class='fa fa-envelope' aria-hidden='true'></i></li>
+                        <li><i class='fa fa-wechat' aria-hidden='true'></i></li>
+                        <li><i class='fa fa-weibo' aria-hidden='true'></i></li>
+                        <li><i class='fa fa-twitter' aria-hidden='true'></i></li>
+                        <li><i class='fa fa-facebook-f' aria-hidden='true'></i></li>
+                        <li><span class="article-shares">{{ $article['shares'] }} shares</span></li>
+                    </ul>
+                </li>
+            @endif
         </ul>
     </li>
-    @endif
 
     <li class='desc' style='clear:both' id="article-desc">{!! $articleDetails['description'] !!}</li>
 
-    <div class="source">
-        <div class="line"></div>
-        <div>資料來源 <span id="article-source">{{ $articleDetails['source'] }}</span></div>
-        <div>PHOTOGRAPHER <span id="article-photographer">{{ $articleDetails['photographer'] }}</span></div>
-    </div>
+    @if($articleDetails['source'] != '' || $articleDetails['photographer'] != '')
+        <div class="source">
+            <div class="line"></div>
+            @if($articleDetails['source'] != "")
+                <div>資料來源 <span id="article-source">{{ $articleDetails['source'] }}</span></div>
+            @endif
+            @if($articleDetails['photographer'] != "")
+                <div>PHOTOGRAPHER <span id="article-photographer">{{ $articleDetails['photographer'] }}</span></div>
+            @endif
+        </div>
+    @endif
 
     @if(!$appMode)
     <div class="tag">
