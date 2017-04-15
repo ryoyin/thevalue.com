f868d0e28d082ac0a4b458a756006d081a36e66b
Roy Ho <rho@thevalue.com>
2017-04-15 22:07:44 +0800

    change dev server address

--- public/js/general.js
+++ public/js/general.js
@@ -2,10 +2,10 @@
 var site_root = window.location.href;
 var sr_split = site_root.split('/');
 
-// console.log(sr_split[2]);
+//console.log(sr_split[2]);
 
 switch(sr_split[2]) {
-    case '192.168.88.102':
+    case '192.168.88.1':
         site_root = 'http://192.168.88.102/thevalue.com/public/';
         var site_lang = (sr_split[5]);
         break;
