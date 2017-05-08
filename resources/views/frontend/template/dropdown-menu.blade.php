0a81e15feb3d09fd8e7e3f99b6469947d192a2fe
Roy Ho <kwanyin2000@gmail.com>
2017-05-08 13:27:25 +0800

    Merge branch 'auction03' of https://github.com/ryoyin/thevalue.com
    # Conflicts:
    #	public/css/web.css
    #	public/css/web.css.map
    #	resources/assets/sass/web.scss
    #	resources/views/frontend/template/dropdown-menu.blade.php
    #	resources/views/frontend/template/head.blade.php

--- resources/views/frontend/template/dropdown-menu.blade.php
+++ resources/views/frontend/template/dropdown-menu.blade.php
@@ -11,14 +11,15 @@
 <nav class="navbar navbar-default navbar-fixed-top head-dropdown-menu">
     <div class="container">
         <div class="navbar-header">
+            <div style="" class="navbar-brand navbar-custom-logo"><a class="navbar-brand" style="float: none;" href="{{ route('frontend.index') }}">THE VALUE</a></div>
             <ul class="nav navbar-nav" style="float: left; text-align: center; position:relative; z-index: 10; width: 100px;">
                 <li class="dropdown lang-list-first">
                     <?php
-                        $current_lang_array = array(
-                            'sim' => '簡',
-                            'en' => 'EN',
-                            'trad' => '繁'
-                        );
+                    $current_lang_array = array(
+                        'sim' => '簡',
+                        'en' => 'EN',
+                        'trad' => '繁'
+                    );
                     ?>
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> <i class="fa fa-globe" aria-hidden="true"></i> {{ $current_lang_array[App::getLocale()] }}</a>
                     <ul class="dropdown-menu">
@@ -28,7 +29,6 @@
                     </ul>
                 </li>
             </ul>
-            <div style="position: absolute; top: 17px; width: 100%; text-align: center; z-index: 17px;"><a class="navbar-brand" style="float: none;" href="{{ route('frontend.index') }}">THE VALUE</a></div>
             <div style="width: 20%; float:right;">
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
                     <span class="sr-only">Toggle navigation</span>
