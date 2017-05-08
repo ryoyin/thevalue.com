3d58c683564c7febb66049e947407792d60cc266
Roy Ho <kwanyin2000@gmail.com>
2017-05-08 19:09:20 +0800

    part 3

--- resources/views/frontend/auction/details/main.blade.php
+++ resources/views/frontend/auction/details/main.blade.php
@@ -6,7 +6,7 @@
 
     <hr style="padding: 0; margin:0">
 
-    <div class="row auction" id="home-content">
+    <div class="row auction auction-detail" id="home-content">
 
         <div id="left">
 
@@ -21,7 +21,54 @@
                 {{--<div class="hidden-md hidden-lg">--}}
                 <div class="">
 
-                    @include('frontend.auction.pre.swiper')
+                    <div class="pre-auction-block">
+                        <div class="store-name"><img src="{{ asset('images/company_logo/christie_logo.jpg') }}"><span>伦敦佳士得</span></div>
+                        <div class="more"><a href="{{ route('frontend.auction.house', ['slug' => 'christies']) }}"">查看更多</a></div>
+                        <div class="series">
+                            <div class="title">拍卖预展 - 2017春季拍卖会</div>
+                            <div class="input-group selection">
+                                <span class="input-group-addon" id="basic-addon1">請選擇 :</span>
+                                <select class="form-control" id="sel1" aria-describedby="basic-addon1">
+                                    <option>中國當代水墨畫 1</option>
+                                    <option>中國當代水墨畫 2</option>
+                                    <option>中國當代水墨畫 3</option>
+                                </select>
+                            </div>
+
+                            <div class="misc">
+                                <div class="cell-name">中國當代水墨畫 1</div>
+
+                                <div style="float:left">2017年05月09日 17:30</div>
+                                <div id="date-counter-1" class="date-counter" style="float:left"></div>
+                                <div style="clear:both"></div>
+                                拍卖地点：<span>8 King Street St. James’s London SW1Y 6QT</span><br>
+                                拍卖总数：<span>136</span> 件<br>
+
+                            </div>
+
+                            <script type="text/javascript">
+                                $("#date-counter-1")
+                                    .countdown("2017/05/09 17:30:00", function(event) {
+                                        $(this).text(
+                                            event.strftime('(%D days %H:%M:%S)')
+                                        );
+                                    });
+                            </script>
+
+                            <div class="row">
+                                <?php for($i=0; $i<10; $i++) { ?>
+                                <div class="col-xs-6 col-md-3 lot">
+                                    <img src="{{ asset('images/auction-p1.jpg') }}" class="img-responsive">
+                                    <div class="lot-detail">
+                                        <div>Lot 102 Title Title Title Title Title Title Title Title Title</div>
+                                        <div>Value: 100000000 -2300000000</div>
+                                    </div>
+                                </div>
+                                <?php } ?>
+                            </div>
+                        </div>
+                    </div>
+
 
                 </div>
 
