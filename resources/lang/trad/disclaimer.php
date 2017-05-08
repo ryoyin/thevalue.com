9695de7d0f8f69eaf572f6762e3bf08e6e91a17d
Roy Ho <kwanyin2000@gmail.com>
2017-05-08 16:25:35 +0800

    update disclaimer

---
+++ resources/lang/trad/disclaimer.php
@@ -0,0 +1,247 @@
+<?php
+
+return [
+
+    'disclaimerContent' => "
+    <h2>Privacy&nbsp;Policy&nbsp;</h2>
+
+
+<p>The Value.com Limited (&ldquo;we&rdquo;, &ldquo;us&rdquo; or &ldquo;our&rdquo;) considers the privacy and protection of your (&ldquo;user&rdquo; or &ldquo;you&rdquo;) data, both corporate and personal, to be of the utmost importance and we take robust measures across our business to protect the security and integrity of all such information.</p>
+<p>To ensure that you feel confident about providing us with your personal information when communicating with us and using our services, we outline below our practices in relation to the collection and use of personal data. This Privacy Policy describes how we will collect, use and process personal data. Please read this Privacy Policy carefully to ensure you understand how we handle your personal data and this Privacy Policy is applicable to both registered and non-registered users. Please also check this page from time to time as the Policy may be updated and your continued access of our website or provision of your personal data to us or continued use of our services following the posting of changes or revisions to the terms of our Privacy Policy will constitute your agreement with and acceptance of the changes or revisions to our Privacy Policy.</p>
+<p>We may collect information from you via your use of our website, www.thevalue.com, its sub-domains, any other websites, media platforms or applications including mobile applications operated/developed by The Value.com Limited (&ldquo;Platforms&rdquo;), when you request information from us or engage our services, or as a result of your relationship with one or more of our clients.</p>
+<p>By submitting information or using any of our Platforms or services and by providing us with any personal information, you are consenting to the use of your personal information as outlined in this policy.&nbsp;&nbsp;Please do not use our Platforms or send us any personal information if you do not want your personal information to be used in this way.</p>
+
+<ol>
+	<li>
+	<h4>INFORMATION COLLECTED THROUGH OUR WEBSITE</h4>
+	</li>
+</ol>
+
+
+<p>We may collect and process the following information about you:</p>
+<ul>
+	<li>
+	<p>information you provide by filling in forms on our Platforms; and information given in hard copy form or by any other means providing: your name; date of birth; log-in ID and password, address; interests; marketing preferences; when appropriate your credit information; and any other personal information relevant to the services we provide. This information may be provided when you are registering interest, transacting with us, subscribing to any of our print or online publications or newsletters or publish information on our Platforms or requesting information;</p>
+	</li>
+	<li>
+	<p>correspondence or other contacts you may have with us;</p>
+	</li>
+	<li>
+	<p><a name=\"_GoBack\"></a> details of transactions you carry out through our Platforms, and any communications by telephone, in person or by any other means relating to those transactions. This may include recording certain telephone conversations; and</p>
+	</li>
+	<li>
+	<p>details of your visits to our Platforms including, but not limited to, traffic data, web logs or other communication data and the resources you access.</p>
+	</li>
+</ul>
+
+<p>The information you provide to us will be used for processing transactions but may also be used for internal data processing, such as compiling statistical information about your individual interests and the use of our websites and apps and your response to our emails and marketing campaigns. As a service to you, we may also use this information to customize the content we offer on our websites and apps.</p>
+
+
+<ol start=\"2\">
+	<li>
+	<h4>PURPOSE OF COLLECTION OF PERSONAL DATA</h4>
+	</li>
+</ol>
+
+<p>In order to have the benefit of and enjoy various services offered by our Platforms, it may be necessary for you to provide us with their personal information and/or data. Although you are not obliged to provide the information and/or data as requested on the Platforms, we will not be able to render certain services on the Platforms in the event that you fail to do so.</p>
+
+<p>Our purposes for the collection of information and data on the Platforms include but not limited to the following:</p>
+
+<ul>
+	<li>
+	To provide you with information, products or services that you request from us or which we believe would interest you in a way that is targeted and relevant. This could include but is not limited to auctions, galleries;
+	</li>
+	<li>
+	T notify you about changes to our products and services;
+	</li>
+	<li>
+	To ensure that our website is presented in the most effective manner for you and your computer;
+	</li>
+	<li>
+	To compile statistics, for example, website usage statistics;
+	</li>
+	<li>
+	(unless you object,) advising you of forthcoming events and services;
+	</li>
+	<li>
+	to identify users who have viewed the Information posted on the Platforms;
+	</li>
+	<li>
+	to identify users who have posted advertisements, materials, messages, photos, views or comments or such other information on the Platforms;
+	</li>
+	<li>
+	to provide the users with services offered by us;
+	</li>
+	<li>
+	to handle and follow up enquiries, requests and complaints;
+	</li>
+	<li>
+	to provide users with event information, marketing and promotional materials for their enjoyment of benefits as members of the Platforms;
+	</li>
+	<li>
+	to identify users who have enjoyed their benefits as members of the Platforms by receiving and using marketing and promotional materials;
+	</li>
+	<li>
+	to provide users with platforms and forum for posting photos, sharing and discussing their insights in respect of services or products related to art;
+	</li>
+	<li>
+	to compile and analyse aggregate statistics about the users&rsquo; use of the Platforms and service usage by the users for our internal use;
+	</li>
+	<li>
+	to facilitate The Value.com Limited to use the users&rsquo; personal data for purposes relating to the provision of services offered by us and marketing services, special events and/or promotions of us and/or our clients; and
+	</li>
+	<li>
+	For other legitimate business purposes.
+	</li>
+</ul>
+
+<p>We strive to only collect personal data which is necessary and adequate but not excessive in relation to the purposes set out hereinabove.</p>
+
+<p>If we require the use of users&rsquo; personal data for a purpose other than those set out hereinabove, we may request the users&rsquo; prescribed consent to the same.</p>
+
+<ol start=\"3\">
+	<li>DISCLOSURE OR TRANSFER OF DATA</li>
+</ol>
+
+<p>We agree to take all practicable steps to keep all your personal data confidential and/or undisclosed.</p>
+
+<p>We will only disclose and/or transfer your personal data to our personnel and staff for the purpose of providing services to you. However, we may disclose and/or transfer such data to third parties under the following circumstances:</p>
+
+<ul>
+	<li>
+	where the information and/or data is disclosed and/or transferred to any third party suppliers or external service providers who have been duly authorized by us to use such information and/or data and who will facilitate the services on the Platforms, including without limitation to the merchants which have subscribed to our online system and are under a duty of confidentiality;</p>
+	</li>
+	<li>
+	where the information and/or data is disclosed and/or transferred to any of our agents or associates who have been duly authorized by us to use such information and/or data;</p>
+	</li>
+	<li>
+	where we need to protect and defend its rights and property;</p>
+	</li>
+	<li>
+	where we consider necessary to do so in order to comply with the applicable laws and regulations, including without limitation compliance with a judicial proceeding, court order, or legal process served on the Platforms; and</p>
+	</li>
+	<li>
+	where we deem necessary in order to maintain and improve the services on the Platforms.</p>
+	</li>
+</ul>
+
+<h4>Personal data collected via the Platforms may be transferred, stored and processed in any country in which we operate. By using the Platforms, you are deemed to have agreed to, consented to and authorized us to disclose and/or transfer your personal information and data under the circumstances stated above, as well as to any transfer of information (including the Information) outside of your country.</h4>
+
+<ol start=\"4\">
+	<li>
+	SUBSCRIPTION OF NEWSLETTER/PROMOTIONAL MATERIALS/MARKETING MATERIALS
+	</li>
+</ol>
+
+<h4>We may from time to time send to members and users of the Platforms, by way of emails and/or short message service (SMS) notifications, newsletters, marketing and promotional materials and information regarding us or our third-party advertisers&rsquo; products, services and the like based on the personal information and data that you have provided to us. We may use your data in direct marketing and we require your consent (which includes an indication of no objection) for that purpose. In this connection, please note that:</h4>
+
+<ul>
+	<li>
+	the name, log-in ID and password, contact details, age, sex, date of birth, country of residence, nationality, education level and work experience of users held by us from time to time may be used by us and/or our authorised personnel or staff in direct marketing;
+	</li>
+	<li>
+	the following classes of services, products and subjects may be marketed:
+	</li>
+</ul>
+
+<ul class='number_list'>
+	<li>
+	a.) special events hosted by us for members and users, including but not limited to courses and workshops;
+	</li>
+	<li>
+	b.) products and services offered by us and our advertisers (the names of such entities can be found in the relevant advertisements and/or promotional or marketing materials for the relevant products and services, as the case may be);
+	</li>
+</ul>
+
+<ul>
+	<li>
+	we may receive money or other property in return for providing the data to the other persons and, when requesting the users&rsquo; written consent, we will inform the users if we receive any money or other property in return for providing the data to the other persons.
+	</li>
+	<li>
+	Suitable measures are implemented to make available to such members the options to &ldquo;opt-out&rdquo; of receiving such materials. In this regard, users may choose to sign up or unsubscribe for such materials by logging into the membership registration or user account maintenance webpage, or clicking on the automatic link appearing in each newsletter/message, or contact us at <a href=\"mailto:itsupports@thevalue.com\">itsupports@thevalue.com</a>.
+	</li>
+</ul>
+
+<ol start=\"5\">
+	<li>
+	COOKIES AND LOG FILES
+	</li>
+</ol>
+
+<p>We do not collect any personally identifiable information from you whilst you visit and browse the Platforms, save and except where such information of you is expressly requested.</p>
+
+<p>When you access the Platforms, we record your visits only and do not collect your personal information or data. The Platforms&rsquo; server software will also record the domain name server address and track the pages you visit and store such information in &ldquo;cookies&rdquo;, and gather and store information like internet protocol (IP) addresses, browser type, referring/exit pages, operating system, date/time stamp, and clickstream data in log files. All these are done without you being aware that they are occurring.</p>
+
+<p>We and third-parties engaged by us use our cookies and third-party cookies, such as the Google Analytics cookies, together to inform, optimize, and serve marketing materials based on your past visits to the Platforms.</p>
+
+<p>We do not link the information and data automatically collected in the above manner to any personally identifiable information. We generally use such automatically collected information and data to estimate the audience size of the Platforms, gauge the popularity of various parts of the Platforms, track users&rsquo; movements and number of entries in our promotional activities and special events, measure users&rsquo; traffic patterns and administer the Platforms. Such automatically collected information and data will not be disclosed save and except in accordance with the Paragraph 3 entitled &ldquo;Disclosure or Transfer of Data&rdquo;.</p>
+
+<ol start=\"6\">
+	<li>
+	GOOGLE ANALYTICS
+	</li>
+</ol>
+
+<p>Google Analytics Advertising Features (&ldquo;Features&rdquo;) are implemented on the Platforms. The Features which we have implemented are based on Google Analytics Demographics and Interest Reporting. Google Analytics is a web analysis service provided by Google which utilizes the data collected to track and examine the use of the Platforms, to prepare reports on the users&rsquo; activities and share them with other Google services. Google may use the data collected to contextualize and personalize the marketing materials of its own advertising network. Users can opt out of Google Analytics at <a href=\"https://tools.google.com/dlpage/gaoptout/\">https://tools.google.com/dlpage/gaoptout/</a>.</p>
+
+<ol start=\"7\">
+	<li>
+	SECURITY
+	</li>
+</ol>
+
+<p>We will take all reasonable and appropriate steps to protect the security and integrity of all personal information provided to our website or apps, or by any other means electronic or otherwise. We will ensure that your personal data within The Value.com Limited is suitably protected against both unauthorised physical and electronic access. These measures include an electronic firewall and various other protection measures that involve virus scanning, installation of security patches, vulnerability testing, backup and recovery planning, employee training, security audits and other steps designed constantly to improve the data protection posture. However, we cannot guarantee that information during transmission through the internet or any computer network is entirely safe from unauthorised intrusion, access or manipulation. We will have no liability for disclosure of information due to errors or unauthorised acts of third parties. In any event, where we believe personal data has been compromised, we will notify you of this.</p>
+
+<ol start=\"8\">
+	<li>
+	THIRD-PARTY WEBSITES
+	</li>
+</ol>
+
+<p>Our websites or apps may contain links to other websites or apps which are not operated by The Value.com Limited. The information you provide to us will not be transmitted to other websites. However, these websites may collect personal information about you in accordance with their own privacy policies. The Value.com Limited is not responsible for the privacy practices or the content of such websites or apps.</p>
+
+<ol start=\"9\">
+	<li>
+	RETENTION OF PERSONAL DATA
+	</li>
+</ol>
+
+<p>Once we have obtained a user&rsquo;s personal information and/or data, it will be maintained securely in our system. Subject to legal requirements, the personal information and/or data of users will be retained by us until the User requests us in writing to erase his/her own personal information and/or data from our database or to terminate his/her membership of the Platforms.</p>
+
+<ol start=\"10\">
+	<li>
+	TRANSFERRING INFORMATION WHEN THERE IS A CHANGE TO OUR BUSINESS
+	</li>
+</ol>
+
+
+<p>If in the future, we re-organise or transfer all or part of our business we may need to transfer your information to new The Value.com Limited entities or to third parties through which the business of The Value.com Limited will be carried out.&nbsp;</p>
+
+<ol start=\"11\">
+	<li>
+	YOUR RIGHTS
+	</li>
+</ol>
+
+<p>Under applicable data protection legislation, we must ensure that your personal information is accurate and up to date.&nbsp;As such, please advise us of any changes to your information.</p>
+<p>You are entitled to request details of the information we hold about you and to have it rectified or deleted as required by applicable data protection legislation. You also have rights to object to some processing and, where we have asked for your consent to process your data, to withdraw this consent. For more information please&nbsp;send an email to&nbsp;<a href=\"mailto:itsupport@thevalue.com\">itsupport@thevalue.com</a>.</p>
+<p>If you receive marketing materials from us, you may withdraw your consent for us to send these to you at any time and free of charge by&nbsp;sending an e-mail to&nbsp;<a href=\"mailto:itsupport@thevalue.com\">itsupport@thevalue.com</a>.</p>
+
+<ol start=\"12\">
+	<li>
+	CHANGES TO THIS PRIVACY POLICY
+	</li>
+</ol>
+
+<p>We reserve the right to amend or update this Privacy Policy and any of our privacy practices at any time. We will notify users of our Platforms by posting any updated policy on this page and such changes will be effective immediately and without further notice. Where appropriate, we may notify you directly of changes to the Privacy Policy either through email or a prominent notice on our website.</p>
+
+<ol start=\"13\">
+	<li>
+	CONTACT
+	</li>
+</ol>
+
+We hope that we can satisfy queries you may have about the way we process your data. If you have any queries in relation to the use and protection of your personal data, please contact us at <a href=\"mailto:itsupport@thevalue.com\">itsupport@thevalue.com</a>.
+"
+
+];
