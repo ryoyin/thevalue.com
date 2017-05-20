dcd65df5ad399edef4a4ecd2b9adda8e31b216ae
Roy Ho <kwanyin2000@gmail.com>
2017-05-20 22:36:52 +0800

    force update css

--- resources/views/frontend/template/head.blade.php
+++ resources/views/frontend/template/head.blade.php
@@ -47,7 +47,7 @@
     <script src="{{ asset('js/photoswipe/photoswipe-ui-default.min.js') }}"></script>
 
     <!-- custom -->
-    <link href="{{ asset('css/web.css') }}?refresh=201705081331" rel="stylesheet">
+    <link href="{{ asset('css/web.css') }}?refresh=201705201036" rel="stylesheet">
 
     <script src="{{ asset('js/js.cookie.js') }}"></script>
 
