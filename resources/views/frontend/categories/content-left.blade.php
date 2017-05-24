b4827d337a8c9bef9c058a3255bcb60aea9094bd
Roy Ho <kwanyin2000@gmail.com>
2017-05-25 00:58:21 +0800

    enhance categories

--- resources/views/frontend/categories/content-left.blade.php
+++ resources/views/frontend/categories/content-left.blade.php
@@ -21,20 +21,19 @@
                             <li class='title'><a href='{{ route('frontend.article', ['slug' => $story['slug']]) }}'>{{ $story['title'] }}</a></li>
                             <li class='published_at'><span>@lang('thevalue.publish-date')</span> {!! $story['published_at'] !!}</li>
                             <li class='desc' style='clear:both'>{!! $story['short_desc'] !!}</li>
-                            <li class='social'>
+                            <li class='social pull-right'>
                                 <ul class='ul-clean share'>
                                     <li><a href="http://service.weibo.com/share/share.php?url={{ route("frontend.article", ['slug' => $story['slug']]) }}=&title={{ $story['title'] }}&pic={{ $image_path }}&ralateUid=&language=zh_cn" target="_blank"><i class='fa fa-weibo' aria-hidden='true'></i></a></li>
                                     <li><a href="http://twitter.com/intent/tweet?text={{ $story['title'] }}&url={{ route("frontend.article", ['slug' => $story['slug']]) }}&pic={{ $image_path }}" target="_blank"><i class='fa fa-twitter' aria-hidden='true'></i></a></li>
                                     <li><a href="http://www.facebook.com/share.php?u={{ route("frontend.article", ['slug' => $story['slug']]) }}&t={{ $story['title'] }}&pic={{ $image_path }}" target="_blank"><i class='fa fa-facebook-f' aria-hidden='true'></i></a></li>
                                 </ul>
                             </li>
-                            <li class='social'>
+                            <li class='social pull-right'>
                                 <ul class='ul-clean share'>
                                     <li class="text">{{ $story['share_counter'] }} <span>@lang('thevalue.shares')</span></li>
                                     <li class="text">{{ $story['hit_counter'] }} <span>@lang('thevalue.hits')</li>
                                 </ul>
                             </li>
-
                         </ul>
                     </div>
                 </div>
