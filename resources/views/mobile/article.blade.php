10facb2161a9c8bc100790087a5e9ebefda1a3d8
Roy Ho <kwanyin2000@gmail.com>
2017-05-25 19:51:05 +0800

    add floor plan to app view

--- resources/views/mobile/article.blade.php
+++ resources/views/mobile/article.blade.php
@@ -2,36 +2,48 @@
 
 @section('content')
 
-    <script src="{{ asset('js/fluidvids.js') }}"></script>
+    @if(strpos($articleDetails, 'exhibition-floor-plan'))
 
-    @if(count($articlePhotos) > 1)
-        @include('frontend.article.carousel')
-    @else
-        <?php
-            if($articlePhotos[0]['s3']) {
-                $image_html_path = config("app.s3_path").$articlePhotos[0]['image_path'];
-            } else {
-                $image_html_path = asset($articlePhotos[0]['image_path']);
-            }
-        ?>
-        <img src="{{ $image_html_path }}" alt="{{ $articlePhotos[0]['alt'] }}" class="img-responsive" onclick="galleryInit(this)">
-    @endif
-
-    {{--@include('frontend.article.carousel')--}}
-
-    <div class="row" id="article-content">
-
-        <div class="col-md-12" id="left">
-            @include('frontend.article.content-left')
+        <div id="article-single-img">
+            <img src="{{ asset($articlePhotos[0]['image_path']) }}" alt="{{ $articlePhotos[0]['alt'] }}" class="img-responsive" onclick="galleryInit(this)">
+        </div>
+        <div class="col-md-12">
+            @include('frontend.article.floorPlan')
         </div>
 
-    </div>
+    @else
 
-    <script>
-        fluidvids.init({
-            selector: ['iframe'], // runs querySelectorAll()
-            players: ['www.youtube.com', 'player.vimeo.com'] // players to support
-        });
-    </script>
+        <script src="{{ asset('js/fluidvids.js') }}"></script>
+
+        @if(count($articlePhotos) > 1)
+            @include('frontend.article.carousel')
+        @else
+            <?php
+                if($articlePhotos[0]['s3']) {
+                    $image_html_path = config("app.s3_path").$articlePhotos[0]['image_path'];
+                } else {
+                    $image_html_path = asset($articlePhotos[0]['image_path']);
+                }
+            ?>
+            <img src="{{ $image_html_path }}" alt="{{ $articlePhotos[0]['alt'] }}" class="img-responsive" onclick="galleryInit(this)">
+        @endif
+
+        {{--@include('frontend.article.carousel')--}}
+
+        <div class="row" id="article-content">
+
+            <div class="col-md-12" id="left">
+                @include('frontend.article.content-left')
+            </div>
+
+        </div>
+
+        <script>
+            fluidvids.init({
+                selector: ['iframe'], // runs querySelectorAll()
+                players: ['www.youtube.com', 'player.vimeo.com'] // players to support
+            });
+        </script>
+    @endif
 
 @endsection
