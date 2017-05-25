17a6b8ce51f26f88e48bff93fd377da884216a21
Roy Ho <kwanyin2000@gmail.com>
2017-05-26 01:42:38 +0800

    rewind infinity scroll

--- resources/views/frontend/homepage/homepage.blade.php
+++ resources/views/frontend/homepage/homepage.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <script src="{{ asset('js/web.js') }}?refresh=2017052601"></script>
+    <script src="{{ asset('js/web.js') }}?refresh=2017052602"></script>
 
     @include('frontend.homepage.carousel')
 
