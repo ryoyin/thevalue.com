5257538b24d7a1bfb25c3da53dfa6739437d1e7e
Roy Ho <kwanyin2000@gmail.com>
2017-05-26 21:11:01 +0800

    android download link

--- resources/views/frontend/homepage/content-right.blade.php
+++ resources/views/frontend/homepage/content-right.blade.php
@@ -2,4 +2,6 @@
 
     @include('frontend.misc.sideBanner')
 
+    <a href="{{ asset('app/android/thevalue-app-release-1.4.apk') }}" class="img-responsive"><img src="{{ asset('images/company_logo/gp-logo.png') }}" style="padding-top: 10px"></a>
+
 </div> <!-- home-right-col -->
--- resources/views/frontend/template/footer.blade.php
+++ resources/views/frontend/template/footer.blade.php
@@ -1,3 +1,4 @@
+<div class="pull-left"><a href="{{ asset('app/android/thevalue-app-release-1.4.apk') }}"><img src="{{ asset('images/company_logo/gp-logo.png') }}" style="height: 40px"></a></div>
 <div id="footer" class="pull-right">
     <a href="{{ route('frontend.aboutus') }}">@lang('thevalue.contact-us')</a> |
     <a href="{{ route('frontend.disclaimer') }}">@lang('thevalue.disclaimer')</a> |
