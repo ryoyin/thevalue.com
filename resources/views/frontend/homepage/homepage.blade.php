a375811d693d8acb970bf5454aec42b93c4ab94c
Roy Ho <kwanyin2000@gmail.com>
2017-05-27 23:42:20 +0800

    update latest & popurlar stories time and force js update

--- resources/views/frontend/homepage/homepage.blade.php
+++ resources/views/frontend/homepage/homepage.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <script src="{{ asset('js/web.js') }}?refresh=2017052604"></script>
+    <script src="{{ asset('js/web.js') }}?refresh=2017052701"></script>
 
     @include('frontend.homepage.carousel')
 
