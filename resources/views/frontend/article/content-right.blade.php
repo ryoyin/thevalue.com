bb63e5617c381b04a2cae3c9cf286ac93b24fa90
Roy Ho <kwanyin2000@gmail.com>
2017-05-31 11:49:49 +0800

    add google adsense at article page

--- resources/views/frontend/article/content-right.blade.php
+++ resources/views/frontend/article/content-right.blade.php
@@ -1,13 +1,13 @@
-{{--<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- Test -->
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- thevalue-auto -->
 <ins class="adsbygoogle"
      style="display:block"
      data-ad-client="ca-pub-8545127753274353"
-     data-ad-slot="3047931022"
+     data-ad-slot="7391805026"
      data-ad-format="auto"></ins>
 <script>
     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>--}}
+</script>
 <div class="header">
     <div class="news pull-left">POPULAR NEWS</div>
     {{--<div class="recent pull-right">Recent <i class="fa fa-chevron-down" aria-hidden="true"></i></div>--}}
