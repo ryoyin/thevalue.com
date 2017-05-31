53d5d110b26da59e7bbe92a3235cc9dc1ca5cd44
Roy Ho <kwanyin2000@gmail.com>
2017-05-31 13:05:08 +0800

    open google adsense

--- resources/views/frontend/template/head.blade.php
+++ resources/views/frontend/template/head.blade.php
@@ -99,12 +99,12 @@
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-TileImage" content="{{ asset("images/icons/ms-icon-144x144.png") }}"> <meta name="theme-color" content="#ffffff">
 
-    {{--<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
     <script>
         (adsbygoogle = window.adsbygoogle || []).push({
             google_ad_client: "ca-pub-8545127753274353",
             enable_page_level_ads: true
         });
-    </script>--}}
+    </script>
 
 </head>
