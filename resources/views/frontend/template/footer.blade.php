7f8884af46c0b0c59a3a840dd854a33a8a0dbd10
Roy Ho <kwanyin2000@gmail.com>
2017-06-17 01:34:40 +0800

    add google adsense at footer

--- resources/views/frontend/template/footer.blade.php
+++ resources/views/frontend/template/footer.blade.php
@@ -1,3 +1,14 @@
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- thevalue-auto -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-8545127753274353"
+     data-ad-slot="7391805026"
+     data-ad-format="auto"></ins>
+<script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
 <div style="text-align: center" class="footer-download-link">
     <ul>
         <li><a href="https://itunes.apple.com/app/the-value/id1204432093" target="_blank"><img src="{{ asset('images/company_logo/app_store_download_icon.png') }}"></a></li>
