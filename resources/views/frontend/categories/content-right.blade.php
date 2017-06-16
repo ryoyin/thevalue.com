cab30b8d8ba40c9fc0284165bb742134bdda73b0
Roy Ho <kwanyin2000@gmail.com>
2017-06-17 01:40:33 +0800

    fix google adsense part 3

--- resources/views/frontend/categories/content-right.blade.php
+++ resources/views/frontend/categories/content-right.blade.php
@@ -1,17 +1,17 @@
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- thevalue-auto -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-8545127753274353"
-     data-ad-slot="7391805026"
-     data-ad-format="auto"></ins>
-<script>
-    (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
-
-<div style="height: 20px"></div>
-
 <div id="right" class="hidden-xs hidden-sm col-md-3">
+
+    {{--<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>--}}
+    <!-- thevalue-auto -->
+    <ins class="adsbygoogle"
+         style="display:block"
+         data-ad-client="ca-pub-8545127753274353"
+         data-ad-slot="7391805026"
+         data-ad-format="auto"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+
+    <div style="height: 20px"></div>
 
     @include('frontend.misc.sideBanner')
 
