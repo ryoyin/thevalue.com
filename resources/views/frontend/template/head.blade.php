59a88357fba9d7ef2e1bb0a911d240134ea9645a
Roy Ho <kwanyin2000@gmail.com>
2017-06-23 16:55:32 +0800

    enahnce article layout margin

--- resources/views/frontend/template/head.blade.php
+++ resources/views/frontend/template/head.blade.php
@@ -47,7 +47,7 @@
     <script src="{{ asset('js/photoswipe/photoswipe-ui-default.min.js') }}"></script>
 
     <!-- custom -->
-    <link href="{{ asset('css/web.css') }}?refresh=2017060801" rel="stylesheet">
+    <link href="{{ asset('css/web.css') }}?refresh=2017062301" rel="stylesheet">
 
     <script src="{{ asset('js/js.cookie.js') }}"></script>
 
