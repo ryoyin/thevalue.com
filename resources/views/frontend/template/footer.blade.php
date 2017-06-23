b5d58cd9de52ad922fd9ab4517c846050a5f66a8
Roy Ho <kwanyin2000@gmail.com>
2017-06-23 13:56:19 +0800

    update apk version

--- resources/views/frontend/template/footer.blade.php
+++ resources/views/frontend/template/footer.blade.php
@@ -16,7 +16,7 @@
         <li><a href="https://itunes.apple.com/app/the-value/id1204432093" target="_blank"><img src="{{ asset('images/company_logo/app_store_download_icon.png') }}"></a></li>
         <li><a href="https://play.google.com/store/apps/details?id=com.thevaluecoreapp" target="_blank"><img src="{{ asset('images/company_logo/gp-logo.png') }}"></a></li>
         <li>
-            <a href="{{ asset('app/android/thevalue-app-release-1.4.apk') }}" style="text-decoration: none; color: #000;">
+            <a href="{{ asset('app/android/thevalue-app-release-v1.5.1.apk') }}" style="text-decoration: none; color: #000;">
                 <img src="{{ asset('images/company_logo/Android_Robot_100.png') }}" class="android-apk-logo"><br>
                 <span>@lang('thevalue.download-android-apk')</span>
             </a>
