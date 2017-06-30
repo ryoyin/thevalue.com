1ac0745a8ce56f27df2c7bd32d28c31c7a31a714
Roy Ho <kwanyin2000@gmail.com>
2017-06-30 19:05:14 +0800

    Sotheby spider success

--- app/Http/Controllers/Backend/Crawler/SothebysController.php
+++ app/Http/Controllers/Backend/Crawler/SothebysController.php
@@ -58,14 +58,38 @@ class SothebysController extends Controller
 
         echo "<br>";
 
+//        exit;
+
+        echo "try: ".$url;
+        echo "<br>";
 
         $cSession = curl_init();
 
         curl_setopt($cSession,CURLOPT_URL,$url);
-        curl_setopt($cSession,CURLOPT_RETURNTRANSFER,true);
-        curl_setopt($cSession,CURLOPT_HEADER, false);
+        curl_setopt($cSession, CURLOPT_HEADER, 0);
+        curl_setopt($cSession,CURLOPT_RETURNTRANSFER,1);
+
+        $headers = [
+            'Accept: application/json, text/javascript, */*; q=0.01',
+//            'Accept-Encoding: gzip, deflate',
+//            'Accept-Language: zh-CN,zh;q=0.8',
+            'Connection: keep-alive',
+            'Content-Length: 0',
+            'Content-Type: application/x-www-form-urlencoded; charset=UTF-8',
+            'Host: www.sothebys.com',
+            'Origin: http://www.sothebys.com',
+            'Referer: http://www.sothebys.com/en/auctions/2017/contemporary-art-evening-auction-l17022.html', //Your referrer address
+            'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+            'X-MicrosoftAjax: Delta=true',
+            'X-Requested-With: XMLHttpRequest',
+        ];
+
+        curl_setopt($cSession,CURLOPT_HTTPHEADER, $headers);
 
         $content=curl_exec($cSession);
+
+        echo $content;
+        exit;
 
         $storePath = 'spider/sothebys/sale/' . $intSaleID . '/';
 
